17c8f0f3f9ce9b31ad9fd60b665c112b2789c92b
dvsmedeiros <dvisson.medeiros@gmail.com>
2018-07-26 19:49:12 -0300

    Initial commit

---
+++ src/main/java/com/dvsmedeiros/reportengine/ReportEngineApplication.java
@@ -0,0 +1,12 @@
+package com.dvsmedeiros.reportengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportEngineApplication.class, args);
+	}
+}
